e426c68a25c53401b51ef75b13d3928865c159ee
rniman <shinmg00@gmail.com>
2022-09-29 20:13:50 +0900

    세이더 이용해서 삼각형 그리기

---
+++ Start_openGL/Shader_program_triangle_2/fragment.glsl
@@ -0,0 +1,13 @@
+#version 330
+//out_Color: 버텍스 세이더에서 입력받는 색상 값
+//FragColor: 출력할 색상의 값으로 프레임 버퍼로 전달 됨. 
+
+//버텍스 세이더에게서 전달 받음
+in vec3 out_Color; 
+//색상 출력
+out vec4 FragColor;
+
+void main(void) 
+{
+	FragColor = vec4 (out_Color , 1.0);
+}
